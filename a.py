3d88a22cd6a5e76555d1112a40d9c670bba07d9c
Akshat Rana <65679227+Akshat511716@users.noreply.github.com>
2021-01-13 15:07:07 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,8 +1,8 @@
 import os
 from twilio.rest import Client
 
-account_sid = 'AC2e730975f3aa395167eecefb23bcc6b3'
-auth_token = '71f106733a370a3eddb79d65f5f7a4bc'
+account_sid = 'x'
+auth_token = 'y'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
